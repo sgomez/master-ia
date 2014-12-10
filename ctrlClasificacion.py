f45969ddaaf9c6306769c0e6fc139718c99640e4
Sergio Gómez <sergio@uco.es>
2014-12-10 08:47:10 +0100

    Fix: Estándar PEP8 y otras mejoras menores

--- ctrlClasificacion.py
+++ ctrlClasificacion.py
@@ -6,6 +6,7 @@ import classification.method as method
 import services
 import views
 
+
 class ctrlClasificacion(QtGui.QMainWindow, views.Ui_MainWindow):
 
     def __init__(self, objetos):
@@ -22,43 +23,50 @@ class ctrlClasificacion(QtGui.QMainWindow, views.Ui_MainWindow):
         self.show()
 
         # Señales
-        self.boxBaseConocimiento.activated.connect(self.boxBaseConocimientoActivatedEvent)
+        self.boxBaseConocimiento.activated.connect(
+            self.boxBaseConocimientoActivatedEvent)
         self.btnClasificar.clicked.connect(self.btnGenerarClickedEvent)
         self.btnSalir.clicked.connect(self.close)
-        self.lstClasesCandidatas.itemClicked.connect(self.lstClasesCandidatasItemClickedEvent)
-
+        self.lstClasesCandidatas.itemClicked.connect(
+            self.lstClasesCandidatasItemClickedEvent)
 
     def boxBaseConocimientoActivatedEvent(self, item):
+        """
+        Actualiza la tabla de atributos y la lista de clases candidatas cuando
+        se selecciona una nueva base de datos de conocimiento
+        :param item: Índice de la base de conocimiento
+        """
         self.objeto = self.objetos[item]
         services.Container().set('clases', self.objeto.clases_candidatas)
         self.generarTablaAtributos()
         self.generarListaClasesCandidatas()
 
-
     def btnGenerarClickedEvent(self):
-        print 'Objeto:', self.objeto.identificador
-        print '================================\n'
+        """
+        Ejecuta la clasificación y actualiza la justificación de las clases
+        seleccionadas
+        """
         metodo = method.Poda(self.objeto)
-        clasesCandidatas, explicacion = metodo.execute()
+        clasesSeleccionadas, explicacion = metodo.execute()
 
         self.txtJustificacionClasificacion.clear()
         self.htmlExplication.setHtml(explicacion)
         self.txtJustificacionClasificacion.setDocument(self.htmlExplication)
         self.txtJustificacionClasificacion.moveCursor(QtGui.QTextCursor.Start)
 
-        clasesSeleccionadas=[]
-        for clase in clasesCandidatas:
-            print '\t ->', clase.nombre
-            clasesSeleccionadas.append(clase.nombre)
-
         self.lstClasesSeleccionadas.clear()
-        self.lstClasesSeleccionadas.addItems(clasesSeleccionadas)
-
+        for clase in clasesSeleccionadas:
+            self.lstClasesSeleccionadas.addItem(clase.nombre)
 
     def cambiaValorAtributoEvent(self, *args):
+        """
+        Actualiza el valor del atributo de una característica del objeto a
+        clasificar
+        :param args: Para QComboBox el índice seleccionado
+        """
+        # item: Corresponde al elemento que envió la señal
         item = self.sender()
         index = item.row
-        value = None
 
         if isinstance(item, QtGui.QLineEdit):
             print "Cambia QLineEdit"
@@ -68,36 +76,41 @@ class ctrlClasificacion(QtGui.QMainWindow, views.Ui_MainWindow):
             value = args[0]
         else:
             raise AttributeError('Tipo de Widget desconocido')
+            value = None
 
         self.objeto.caracteristicas[index].set_valor(value)
 
-        for at in self.objeto.caracteristicas:
-            print at.atributo.nombre, at.valor
-
+        for caracteristica in self.objeto.caracteristicas:
+            print caracteristica.atributo.nombre, caracteristica.valor
 
     def generarListaClasesCandidatas(self):
+        """
+        Para la base de conocimiento activa genera la lista de clases
+        candidatas
+        :return:
+        """
         self.lstClasesCandidatas.clear()
-        clases_candidatas=services.Container().get('clases')
+        clases_candidatas = services.Container().get('clases')
+
         for clase in clases_candidatas:
             self.lstClasesCandidatas.addItem(clase.nombre)
 
         self.lstClasesCandidatas.setCurrentRow(0)
 
-
     def generarTablaAtributos(self):
+        """
+        Para la base de conocimiento activa genera la tabla de atributos
+        :return:
+        """
         self.tblObjeto.clear()
         self.tblObjeto.setRowCount(len(self.objeto.caracteristicas))
         self.tblObjeto.setHorizontalHeaderLabels(['Atributo', 'Valor'])
 
-        #print observables
         for i, at in enumerate(self.objeto.caracteristicas):
-            print "Característica: Atributo=%s Valor=%s" % (at.atributo.nombre, at.valor)
-            print  at.atributo.nombre,at.atributo.tipo, at.valor,type(at.valor),at.atributo.unidad#,
-
             # Atributo
             celda_nombre = QtGui.QTableWidgetItem(at.atributo.nombre)
-            celda_nombre.setFlags(QtCore.Qt.ItemIsUserCheckable|QtCore.Qt.ItemIsEnabled)
-            self.tblObjeto.setItem(i, 0, celda_nombre)
+            celda_nombre.setFlags(
+                QtCore.Qt.ItemIsUserCheckable | QtCore.Qt.ItemIsEnabled)
 
             # Valor
             celda_valor = QtGui.QLineEdit()
@@ -105,27 +118,26 @@ class ctrlClasificacion(QtGui.QMainWindow, views.Ui_MainWindow):
 
             if at.atributo.tipo == 'boleano':
                 celda_valor = QtGui.QComboBox()
-                celda_valor.row = i
-                celda_valor.addItem('False')
-                celda_valor.addItem('True')
                 celda_valor.activated.connect(self.cambiaValorAtributoEvent)
-                if at.valor == True:
-                    celda_valor.setCurrentIndex(1)
+                celda_valor.addItems(['False', 'True'])
+                celda_valor.row = i
+                celda_valor.setCurrentIndex(1 if at.valor is True else 0)
             elif at.atributo.tipo in ['int', 'float', 'str']:
                 celda_valor.setText(str(at.valor))
-                celda_valor.editingFinished.connect(self.cambiaValorAtributoEvent)
+                celda_valor.editingFinished.connect(
+                    self.cambiaValorAtributoEvent)
             else:
-                raise AttributeError('Tupo de atributo desconocido')
+                raise AttributeError('Tipo de atributo desconocido')
 
+            self.tblObjeto.setItem(i, 0, celda_nombre)
             self.tblObjeto.setCellWidget(i, 1, celda_valor)
-
 
     def lstClasesCandidatasItemClickedEvent(self):
         row = self.lstClasesCandidatas.currentRow()
         self.txtDescripcionTareas.clear()
-        self.htmlDescripcionClase.setHtml(self.objeto.clases_candidatas[row].description())
+        self.htmlDescripcionClase.setHtml(
+            self.objeto.clases_candidatas[row].description())
         self.txtDescripcionTareas.setDocument(self.htmlDescripcionClase)
-
 
     def setupUi(self, MainWindow):
         views.Ui_MainWindow.setupUi(self, MainWindow)
